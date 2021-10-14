49a54c8c3b64562045cd9d507540c6b12baa55d3
prynge <37028634+prynge@users.noreply.github.com>
2021-10-14 10:45:49 +0200

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,11 +16,11 @@ class SecurityController extends AbstractController
      */
     public function login(IriConverterInterface $iriConverter)
     {
-        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $this->json([
-                'error' => 'Invalid login request: check that the Content-Type header is "application/json".'
-            ], 400);
-        }
+        // if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+        //     return $this->json([
+        //         'error' => 'Invalid login request: check that the Content-Type header is "application/json".'
+        //     ], 400);
+        // }
 
         return new Response(null, 204, [
             'Location' => $iriConverter->getIriFromItem($this->getUser())
