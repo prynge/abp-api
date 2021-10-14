43cecf2dc4f12d60764acdbfffb9ce08ff214dd7
prynge <37028634+prynge@users.noreply.github.com>
2021-10-14 05:21:16 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,12 +5,6 @@ use App\Kernel;
 // header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
-$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
-if($trustedProxies) {
-    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
-} 
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
